79fdc5ad59b8ac36dabe6e115160735eb014c9c6
Edward Betancourt <edward@shelfiehq.com>
2015-07-27 19:57:37 -0400

    Learning more directives and adding more style.

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -21,29 +21,42 @@ angular.module('store.view1', ['ui.router'])
             {
                 'name'          : 'Diamond',
                 'description'   : 'This is a beautiful gem!',
-                'price'         : '$1,200.00',
+                'price'         : 1200,
                 'quantity'      : 5,
                 'canPurchase'   : true,
-                'soldOut'       : false
+                'soldOut'       : false,
+                'image'         : 'http://www.shopnaser.com/images/diamonds_facts.png'
             },
             {
                 'name'          : 'Ruby',
                 'description'   : 'This is bright red!',
-                'price'         : '$800.00',
+                'price'         : 800,
                 'quantity'      : 4,
                 'canPurchase'   : true,
-                'soldOut'       : false
+                'soldOut'       : false,
+                'image'         : 'https://devops.profitbricks.com/static/img/logo-ruby.png'
             },
             {
-                'name'          : 'Esmeralda',
+                'name'          : 'Emerald',
                 'description'   : 'This is a nice green!',
-                'price'         : '$900.00',
+                'price'         : 900,
                 'quantity'      : 10,
                 'canPurchase'   : false,
-                'soldOut'       : false
+                'soldOut'       : false,
+                'image'         : 'https://pbs.twimg.com/profile_images/1811370612/emerald.jpg'
             }
         ];
 
+        // put together some code to sort the collection
+
 }]);
 
-//http://localhost:7000/#/view1
+// Diamond
+//http://www.shopnaser.com/images/diamonds_facts.png
+
+// Emerald
+// https://pbs.twimg.com/profile_images/1811370612/emerald.jpg
+
+// Ruby
+//https://devops.profitbricks.com/static/img/logo-ruby.png
+
